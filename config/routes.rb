ff993aad113840d91f0263f45198d318dc1c3336
Matteo <matteoratti90@mail.com>
2018-03-21 16:42:57 +0100

    set main route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :operators
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'operators#index'
+
   get '/start', to: 'operators#start'
 end
