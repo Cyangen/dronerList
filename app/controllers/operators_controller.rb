8f3cd009411419d4371fb1db99aabb95f11be583
Matteo <matteoratti90@mail.com>
2018-03-21 17:19:42 +0100

    remove droners to index no search

--- app/controllers/operators_controller.rb
+++ app/controllers/operators_controller.rb
@@ -11,46 +11,46 @@ class OperatorsController < ApplicationController
     if params[:operator]
       @operators = Operator.where('address LIKE ?', "%#{params[:operator]}%")
     else
-      @operators = Operator.take(20)
+      @operators = nil
     end
 
   end
 
   def start
-    mechanize = Mechanize.new
-    page_number = 1
-
-    23.times do
-      doc = Nokogiri::HTML(open("https://www.dronezine.it/database-operatori-droni-e-costruttori-uav/visualizzazione-database-operatori-professionali-droni-e-uav/?listpage=" + page_number.to_s))
-
-      table = doc.at('table')
-      table.search('tr').each do |tr|
-        cells = tr.search('th, td')
-        name = cells[0].text.strip
-        address = cells[1].text.strip + ' ' + cells[2].text.strip
-        info = cells[5].text.strip
-        website = cells[4].text.strip
-
-        Operator.create(name: name.downcase, infos:info.downcase, website:website.downcase, address:address.downcase)
-      end
-
-      page_number = page_number + 1
-    end
-
-    doc2 = Nokogiri::HTML(open("https://www.dronezine.it/piloti/"))
-    table2 = doc2.at('table')
-
-    table2.search('tr').each do |tr|
-      cells = tr.search('th, td')
-      name = cells[2].text.strip + cells[3].text.strip
-      address = cells[1].text.strip + ' ' + cells[0].text.strip
-      phone = cells[4].text.strip
-      pec = cells[5].text.strip
-
-      unless Operator.exists?(name: name)
-        Operator.create(name: name.downcase, pec: pec.downcase, phone:phone, address:address.downcase)
-      end
-    end
+    # mechanize = Mechanize.new
+    # page_number = 1
+    #
+    # 23.times do
+    #   doc = Nokogiri::HTML(open("https://www.dronezine.it/database-operatori-droni-e-costruttori-uav/visualizzazione-database-operatori-professionali-droni-e-uav/?listpage=" + page_number.to_s))
+    #
+    #   table = doc.at('table')
+    #   table.search('tr').each do |tr|
+    #     cells = tr.search('th, td')
+    #     name = cells[0].text.strip
+    #     address = cells[1].text.strip + ' ' + cells[2].text.strip
+    #     info = cells[5].text.strip
+    #     website = cells[4].text.strip
+    #
+    #     Operator.create(name: name.downcase, infos:info.downcase, website:website.downcase, address:address.downcase)
+    #   end
+    #
+    #   page_number = page_number + 1
+    # end
+    #
+    # doc2 = Nokogiri::HTML(open("https://www.dronezine.it/piloti/"))
+    # table2 = doc2.at('table')
+    #
+    # table2.search('tr').each do |tr|
+    #   cells = tr.search('th, td')
+    #   name = cells[2].text.strip + cells[3].text.strip
+    #   address = cells[1].text.strip + ' ' + cells[0].text.strip
+    #   phone = cells[4].text.strip
+    #   pec = cells[5].text.strip
+    #
+    #   unless Operator.exists?(name: name)
+    #     Operator.create(name: name.downcase, pec: pec.downcase, phone:phone, address:address.downcase)
+    #   end
+    # end
   end
 
   # GET /operators/1
